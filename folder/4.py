082a3af06bc8fd6d4830f4ad439ae77738aef3ff
lisusuy <2021212046@bupt.cn>
2023-09-07 11:41:42 +0800

    V1.0.2

--- folder/4.py
+++ folder/4.py
@@ -1,13 +1,13 @@
 import os
 import pickle
 
-# 规则1：匹配os
+
 def execute_os_command():
     command = "echo 'os command'"
     result = os.system(command)
     print(f"Result of executing OS command: {result}")
 
-# 规则3：匹配pickle.loads
+
 def deserialize_pickle_data():
     serialized_data = b'\x80\x03X\x0b\x00\x00\x00Hello, World!q\x00.'
     obj = pickle.loads(serialized_data)
